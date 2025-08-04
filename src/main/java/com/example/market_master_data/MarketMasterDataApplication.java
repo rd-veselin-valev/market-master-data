86b1f1626953f5183be5d2b240e027006e7ad497
Veselin-Valev-Rewe <veselin.valev@rewe-group.com>
2025-08-04 10:22:24 +0300

    Initial commit

---
+++ src/main/java/com/example/market_master_data/MarketMasterDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.market_master_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketMasterDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketMasterDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/market_master_data/MarketMasterDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.market_master_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketMasterDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
